ba31c2cf0eeb1636b2e38ffc916c942c1206e08d
John Bramlett <jbramlett@3street.net>
2017-06-11 09:17:59 -0400

    Adding manual creation of permutations

--- src/com/sirius/permutations/permutator.py
+++ src/com/sirius/permutations/permutator.py
@@ -52,6 +52,49 @@ class Permutator(object) :
         logger.info("Generating permutations for set %s", str(number_set))
         return [list(x) for x in itertools.permutations(number_set)]
 
+    def get_permutations_manual(self, id):
+        """
+        Instead of using build-in permutation generator, do it by hand
+
+        :param id: The id of the set we are generating the permutations for
+        :return: A list of lists containing the permutations of the original list
+        """
+        number_set = self.get_set(id)
+
+        result = []
+        self._permutate(number_set[:], 0, result)
+
+        return result
+
+
+    def _permutate(self, number_set, start, result):
+        """
+        Recursive routine used to build our permutations
+
+        :param number_set: The original number set
+        :param start: The start position from which to do our swap
+        :param result: The resulting permutations
+        """
+        if start >= len(number_set):
+            result.append(number_set[:])
+
+        for x in range(start, len(number_set)) :
+            self._swap(number_set, start, x);
+            self._permutate(number_set, start + 1, result);
+            self._swap(number_set, start, x);
+
+    def _swap(self, number_set, s1, s2):
+        """
+        Swaps the values at the given positions
+
+        :param number_set: The numbers list
+        :param s1: The first position
+        :param s2:  The second position
+        """
+        first = number_set[s2]
+        number_set[s2] = number_set[s1]
+        number_set[s1] = first
+
     def get_sum(self, id):
         """
         Get the sum of all the numbers across all permutations
